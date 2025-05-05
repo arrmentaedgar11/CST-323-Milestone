11e50cc1ed9a827dfc90c3656d1a2c8346d94f5e
Charles Ackley <ackleycharlie@outlook.com>
2025-05-04 21:54:00 -0400

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,3 +5,28 @@ CREATE TABLE `products` (
   `description` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE claims (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    businessName VARCHAR(100),
+    orderNumber BIGINT,
+    returnReason VARCHAR(100),
+    claimStatus VARCHAR(100),
+    preferredResolution VARCHAR(250),
+    businessResponse VARCHAR(100),
+    approvedResolutionType VARCHAR(100),
+    returnInstructions VARCHAR(100),
+    claimActive BOOLEAN
+);
+
+CREATE TABLE warranties (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    warrantyName VARCHAR(100),
+    warrantyDesc VARCHAR(250),
+    productCategory VARCHAR(100),
+    restockFees DECIMAL,
+    email VARCHAR(250),
+    phoneNumber VARCHAR(20),
+    returnInstructions VARCHAR(250),
+    expirationDate BIGINT
+);
