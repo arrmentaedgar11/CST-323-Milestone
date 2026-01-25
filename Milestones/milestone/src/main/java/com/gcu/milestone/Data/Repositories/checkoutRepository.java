4048899c13cf0410d4093bc3cb92e775964d382e
arrmentaedgar11 <armentaedgar11@gmail.com>
2026-01-25 13:40:46 -0800

    Fix H2 schema and checkout mapping

--- Milestones/milestone/src/main/java/com/gcu/milestone/Data/Repositories/checkoutRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gcu.milestone.Data.Repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.gcu.milestone.Data.Entities.CheckoutEntity;
-
-public interface checkoutRepository extends CrudRepository<CheckoutEntity, Long> {
-
-}
--- Milestones/milestone/src/main/java/com/gcu/milestone/Data/Repositories/movieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gcu.milestone.Data.Repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.gcu.milestone.Data.Entities.MovieEntity;
-
-public interface movieRepository extends CrudRepository<MovieEntity, Long>{
-    
-}
